c953109b8102dc4af30e25becf945143ea65e144
Ihor <likerrr777@gmail.com>
2019-05-13 23:57:37 +0300

    Adding stakeholders section (save work).

---
+++ components/communitiesImpacted/stakeholders/stakeholders.js
@@ -0,0 +1,10 @@
+angular.module('MainApp')
+.component('stakeholders', {
+      templateUrl: 'components/communitiesImpacted/stakeholders/stakeholders.html',
+      bindings: {
+         data: '<'
+      },
+      controller: function() {
+         
+      }
+})
