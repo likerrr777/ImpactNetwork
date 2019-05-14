825f1417b8edbd8e56f78a1dcd1b2f56be0b28b8
eboks\iro <iro@e-Boks.dk>
2019-05-14 16:10:02 +0300

    Adding new component: save work

--- components/communitiesImpacted/stakeholders/stakeholders.js
+++ components/communitiesImpacted/stakeholders/stakeholders.js
@@ -1,10 +1,26 @@
-angular.module('MainApp')
-.component('stakeholders', {
-      templateUrl: 'components/communitiesImpacted/stakeholders/stakeholders.html',
-      bindings: {
-         data: '<'
-      },
-      controller: function() {
-         
-      }
-})
+function StakeholdersController() {
+  this.$onInit = function() {
+    var ctrl = this;
+
+    setCurrentStakeholder(0);
+    ctrl.sroiMultiplier = 1;
+    ctrl.totalDollarValue = calculateTotalDollarValue();
+
+    function setCurrentStakeholder(index) {
+      ctrl.currentStakeholderIndex = index;
+      ctrl.currentStakeholder = ctrl.stakeholders.data[index];
+    }
+
+    function calculateTotalDollarValue() {
+      return 0;
+    }
+  };
+}
+
+angular.module("MainApp").component("stakeholders", {
+  templateUrl: "components/communitiesImpacted/stakeholders/stakeholders.html",
+  controller: StakeholdersController,
+  bindings: {
+    stakeholders: "<"
+  }
+});
