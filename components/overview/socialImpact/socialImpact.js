56b07fd52c19734f8c442cf02ffea7ed83fdf84e
Ihor <likerrr777@gmail.com>
2019-05-15 23:00:54 +0300

    save work

--- components/overview/socialImpact/socialImpact.js
+++ components/overview/socialImpact/socialImpact.js
@@ -6,7 +6,35 @@ class SocialImpactController {
     let StakeholdersController = StakeholdersComponent.controller;
     this.stakeholdersController = new StakeholdersController();
 
+    this.sroiMultiplier = 1;
+    this.selectedYear = this.yearsData[0];
+    this.selectedDataset = this.selectedYear.dataset[0];
+
     this.stakeholdersController.stakeholders = {}; //todo
+  }
+
+  changeYear(year) {
+    this.selectedYear = year;
+  }
+
+  changeDataset(dataset) {
+    this.selectedDataset = dataset;
+
+    this.positiveQuantifiablesNumber = this.selectedDataset.stakeholders.reduce((accumulator, stakeholder) => {
+      return accumulator + stakeholder.data.quantifiables.filter(p => p.isPositive).length;
+    }, 0);
+
+    this.negativeQuantifiablesNumber = this.selectedDataset.stakeholders.reduce((accumulator, stakeholder) => {
+      return accumulator + stakeholder.data.quantifiables.filter(p => !p.isPositive).length;
+    }, 0);
+
+    this.positiveNonQuantifiablesNumber = this.selectedDataset.stakeholders.reduce((accumulator, stakeholder) => {
+      return accumulator + stakeholder.data.nonQuantifiables.filter(p => p.isPositive).length;
+    }, 0);
+
+    this.negativeNonQuantifiablesNumber = this.selectedDataset.stakeholders.reduce((accumulator, stakeholder) => {
+      return accumulator + stakeholder.data.nonQuantifiables.filter(p => !p.isPositive).length;
+    }, 0);
   }
 
   $onChanges(changesObj) {
@@ -18,6 +46,7 @@ export default {
   templateUrl: "components/overview/socialImpact/socialImpact.html",
   controller: SocialImpactController,
   bindings: {
-    socialImpact: "<"
+    yearsData: "<",
+    staticResources: "<"
   }
 };
